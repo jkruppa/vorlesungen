90ec31efd93f09c32e56570f57382f67c3c11721
Jochen Kruppa <jkruppa@users.noreply.github.com>
2026-06-04 11:52:32 +0200

    added

--- Allgemeine Daten/covariate/flea_jumplength_bloodmeal.R
+++ Allgemeine Daten/covariate/flea_jumplength_bloodmeal.R
@@ -1,9 +1,29 @@
 pacman::p_load(tidyverse, readxl, writexl, conflicted)
 
-jumplength_bloodmeal_tbl <- read_excel("~/Desktop/flea_jumplength_bloodmeal_raw.xlsx", range = "A1:E22")
+## small dataset
+jumplength_bloodmeal_tbl <- read_excel("~/work/GitHub/vorlesungen/Allgemeine Daten/covariate/flea_jumplength_bloodmeal_raw.xlsx", range = "A1:E15")
+
+jumplength_bloodmeal_tbl |> 
+  sample_frac() |> 
+  mutate(fid = 1:14,
+         bodyweight = round(bodyweight, 1)) |> 
+  select(-satisfaction) |> 
+  write_xlsx("~/work/GitHub/vorlesungen/Allgemeine Daten/covariate/flea_jumplength_bloodmeal_n14.xlsx")
+
+## full data set
+jumplength_bloodmeal_tbl <- read_excel("~/work/GitHub/vorlesungen/Allgemeine Daten/covariate/flea_jumplength_bloodmeal_raw.xlsx", range = "A1:E22")
 
 jumplength_bloodmeal_tbl |> 
   sample_frac() |> 
   mutate(fid = 1:21,
          bodyweight = round(bodyweight, 1)) |> 
-  write_xlsx("~/Desktop/flea_jumplength_bloodmeal.xlsx")
+  select(-satisfaction) |> 
+  write_xlsx("~/work/GitHub/vorlesungen/Allgemeine Daten/covariate/flea_jumplength_bloodmeal_n21.xlsx")
+
+jumplength_bloodmeal_tbl |> 
+  sample_frac() |> 
+  mutate(fid = 1:21,
+         bodyweight = round(bodyweight, 1)) |> 
+  mutate(satisfaction = case_when(bloodmeal < 4 ~ "hungry",
+                                  bloodmeal >= 4 & bloodmeal < 8 ~ "full",
+                                  bloodmeal >= 8 ~ "overeat"))
